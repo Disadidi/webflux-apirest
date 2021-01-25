c34f1457323cadbeed9a6d361440b4d3d16a8632
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-25 19:01:01 +0100

    ajout d'handlerProducto, pour s'en servir de controller

--- src/main/java/com/hdconsulting/springboot/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/hdconsulting/springboot/webflux/app/RouterFunctionConfig.java
@@ -1,34 +1,25 @@
 package com.hdconsulting.springboot.webflux.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import com.hdconsulting.springboot.webflux.app.models.documents.Producto;
-import com.hdconsulting.springboot.webflux.app.models.services.ProductoService;
-
-
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import com.hdconsulting.springboot.webflux.app.handler.ProductoHandler;
 
 
 @Configuration
 public class RouterFunctionConfig {
 	
-	@Autowired
-	private ProductoService service;
+	
 	
 	@Bean
-	public RouterFunction<ServerResponse> routes(){
+	public RouterFunction<ServerResponse> routes(ProductoHandler handler){
 		
-		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")), request ->{
-			return ServerResponse.ok()
-					.contentType(MediaType.APPLICATION_JSON)
-					.body(service.findAll(), Producto.class);
-		});
+		return route(GET("/api/v2/productos").or(GET("/api/v3/productos")), request -> handler.listar(request) );
 	}
 
 }
---
+++ src/main/java/com/hdconsulting/springboot/webflux/app/handler/ProductoHandler.java
@@ -0,0 +1,28 @@
+package com.hdconsulting.springboot.webflux.app.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.hdconsulting.springboot.webflux.app.models.documents.Producto;
+import com.hdconsulting.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductoHandler {
+	
+	@Autowired
+	private ProductoService service;
+	
+	public Mono<ServerResponse> listar(ServerRequest request){
+		
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(service.findAll(), Producto.class);
+		
+	}
+
+}
