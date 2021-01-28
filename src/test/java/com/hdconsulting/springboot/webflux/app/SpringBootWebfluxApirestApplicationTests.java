eca1d287a5743704bb9d5bddd771959232b68866
Hugo Disadidi <hdisadidiconsulting@gmail.com>
2021-01-28 06:37:04 +0100

    debut de test et correction validation

--- src/test/java/com/hdconsulting/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/hdconsulting/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.hdconsulting.springboot.webflux.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.hdconsulting.springboot.webflux.app.models.documents.Producto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
-	void contextLoads() {
+	void listarTest() {
+		
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()//pour lancer
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(Producto.class)
+		.hasSize(9)
+		;
 	}
 
 }
